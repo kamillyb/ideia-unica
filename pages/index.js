cc1cdda774a3c9a69b9b2711545affe8399a703f
Kamilly Birkner <ti2@woelfer.com.br>
2024-05-02 16:30:00 -0300

    nova home

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,3 @@
-import { useState } from "react"
-
-function Home(){
-    return (
-    <div>
-        <h1>Home</h1>
-        <Contador/>
-    </div>
-    )
-}
-
-function Contador(){
-    const[Contador,setContador] = useState(1);
-
-    function adicionarContador(){
-        setContador(Contador+1);
-    }
-    return(
-        <div>
-            <div>{Contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-}
+import { Home } from "./Home"
 
 export default Home
